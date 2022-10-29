a460f3d9daa72a2d3351336cb805de5687ad1a6a
dannyjyc <13080839167@163.com>
2022-10-29 21:10:41 +0800

    修复修改为动态路由后，开发环境terminal会出现异常的问题

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,10 +1,30 @@
-import Test from '@/page'
+import React,{lazy} from 'react'
+// import Test from '@/page'
+
+function __variableDynamicImportRuntime__(path) {
+    switch (path) {
+      case '':
+        return import('@/page');
+      default:
+        return new Promise(function (resolve, reject) {
+          queueMicrotask(reject.bind(null, new Error('Unknown variable dynamic import: ' + path)));
+        });
+    }
+  }
+
+const lazyLoad = (path)=>{
+    const Comp = lazy(()=>__variableDynamicImportRuntime__(path))
+    return (
+        <React.Suspense fallback={<>加载中···</>}>
+            <Comp />
+        </React.Suspense>
+    )
+}
 
 let router = [
   {
-    id:'1',
     path: "/",
-    element: <Test/>,
+    element: lazyLoad(''),
   }
 ];
 

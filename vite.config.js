f438acd02047cbf8680439d4c0d6e133d3554564
dannyjyc <13080839167@163.com>
2022-10-31 10:25:30 +0800

    引入vite-plugin-style-import处理打包时全部引用导致有过大包的问题

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import styleImport from 'vite-plugin-style-import';
 
 import url from 'url';
 import path from 'path'
@@ -10,7 +11,27 @@ console.log(__filename2);
 console.log(__dirname2);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'antd',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `antd/es/${name}/style/index`;
+          }
+        }
+      ],
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      }
+    }
+  },
   resolve: {
     //配置别名 @为src 引入
     alias: {

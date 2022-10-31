80652ad95d95e143874e1dd596906205a515668c
dannyjyc <13080839167@163.com>
2022-10-31 09:15:13 +0800

    尝试引入antd

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import Home from '@/components/Home'
+import { Breadcrumb, Layout, Menu } from "antd";
+import { useState } from "react";
+import reactLogo from "@/assets/react.svg";
+import Home from "@/components/Home";
 
-function index(){
-    const [count, setCount] = useState(0)
+const { Header, Content, Footer } = Layout;
+import "@/static/style/index.css";
 
-    return (
-      <div className="App">
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://reactjs.org" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+function index() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Layout>
+      <Header style={{ position: "fixed", zIndex: 1, width: "100%" }}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["2"]}
+          items={new Array(3).fill(null).map((_, index) => ({
+            key: String(index + 1),
+            label: `nav ${index + 1}`,
+          }))}
+        />
+      </Header>
+      <Content
+        className="site-layout"
+        style={{ padding: "0 50px", marginTop: 64 }}
+      >
+        <Breadcrumb style={{ margin: "16px 0" }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div
+          className="site-layout-background"
+          style={{ padding: 24, minHeight: 380 }}
+        >
+          <div className="App">
+            <div>
+              <a href="https://vitejs.dev" target="_blank">
+                <img src="/vite.svg" className="logo" alt="Vite logo" />
+              </a>
+              <a href="https://reactjs.org" target="_blank">
+                <img src={reactLogo} className="logo react" alt="React logo" />
+              </a>
+            </div>
+            <h1>Vite + React</h1>
+            <Home></Home>
+            <div className="card">
+              <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+              </button>
+              <p>
+                Edit <code>src/App.jsx</code> and save to test HMR
+              </p>
+            </div>
+            <p className="read-the-docs">
+              Click on the Vite and React logos to learn more
+            </p>
+          </div>
         </div>
-        <h1>Vite + React</h1>
-        <Home></Home>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </div>
-    )
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+      </Footer>
+    </Layout>
+  );
 }
 export default index;

dfc11e46b25b50b110b7552fa0bdb950bcc6fba7
dannyjyc <13080839167@163.com>
2022-11-10 16:38:29 +0800

    引入antd grid提交目前更改

--- src/page/content/detail.jsx
+++ src/page/content/detail.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
+import { Col, Row } from 'antd';
 
 const ContentDetail = () => {
   const params = useParams();
   return (
-    <>
-      <p>{params.id}</p>
-      <ReactMarkdown># Hello, *world*!</ReactMarkdown>
-    </>
+    <Row>
+      <Col span={18} offset={3} className="">
+        col-18 col-offset-3
+        <p>{params.id}</p>
+        <ReactMarkdown># Hello, *world*!</ReactMarkdown>
+      </Col>
+    </Row>
   );
 };
 

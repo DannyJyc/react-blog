0365e36cbf8ec4efc382486f76e17a7f18be307a
dannyjyc <13080839167@163.com>
2022-11-14 15:40:03 +0800

    引入quill富文本编辑器，dompurify渲染html防止xss

--- src/page/content/detail.jsx
+++ src/page/content/detail.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import ReactMarkdown from "react-markdown";
-import { Col, Row } from 'antd';
+import { Col, Row } from "antd";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import purify from "dompurify";  
 
 const ContentDetail = () => {
+  const [value, setValue] = useState("");
   const params = useParams();
   return (
-    <Row>
-      <Col span={18} offset={3} className="">
-        col-18 col-offset-3
-        <p>{params.id}</p>
-        <ReactMarkdown># Hello, *world*!</ReactMarkdown>
-      </Col>
-    </Row>
+    <>
+      <Row>
+        <Col span={18} offset={3} className="">
+          col-18 col-offset-3
+          <ReactQuill theme="snow" value={value} onChange={setValue} />
+        </Col>
+      </Row>
+      <Row>
+        <Col span={18} offset={3} className="">
+          <p>{params.id}</p>
+          <div dangerouslySetInnerHTML={{ __html:purify.sanitize(value) }} /> 
+        </Col>
+      </Row>
+    </>
   );
 };
 
